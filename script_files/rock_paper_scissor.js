23c4efbca8aabdea43bcfd24d8469b7d061f99b6
dipak1920basnet <dipak1920basnet@gmail.com>
2025-08-12 13:56:13 +0545

    gets computer move

--- script_files/rock_paper_scissor.js
+++ script_files/rock_paper_scissor.js
@@ -1,2 +1,9 @@
-console.log("Hello world!")
-console.log("Greetings from console")
+function getComputerChoice(){
+    let computer_choice = ["rock","paper","scissor"]
+    let choice = computer_choice[Math.floor(Math.random()*computer_choice.length)];
+    return choice
+}
+
+
+computer_choices = getComputerChoice()
+console.log(computer_choices)
